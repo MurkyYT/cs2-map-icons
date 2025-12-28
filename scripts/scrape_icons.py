d01d020499a26b4f85ae7e8638766856da519389
Murky <71258936+MurkyYT@users.noreply.github.com>
2025-12-28 18:09:34 +0200

    feat(cpu_count): add workaround for linux action runner

--- scripts/scrape_icons.py
+++ scripts/scrape_icons.py
@@ -1,11 +1,5 @@
+import os, requests, sys, json, time, hashlib, csv, subprocess
 from playwright.sync_api import sync_playwright
-import os
-import requests
-import sys
-import json
-import time
-import hashlib
-import csv
 from loguru import logger
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from threading import Lock
@@ -17,6 +11,21 @@ data_lock = Lock()
 
 repo = os.getenv("GITHUB_REPOSITORY", "MurkyYT/cs2-map-icons")
 default_branch = os.getenv("DEFAULT_BRANCH", "main")
+
+def get_cpu_count():
+    count = os.cpu_count()
+    if count is not None:
+        return count
+    
+    if sys.platform == "linux":
+        try:
+            cmd = "lscpu | grep '^CPU(s):' | awk '{print $2}'"
+            output = subprocess.check_output(cmd, shell=True, text=True)
+            return int(output.strip())
+        except Exception:
+            pass
+            
+    return None
 
 def is_official(map_name):
     return map_name.lower()[0:3] in ["de_", "dz_", "gd_", "cs_", "ar_"]
@@ -191,7 +200,7 @@ def download_all_icons(existing_data):
 
     existing_maps = existing_data.get("maps", {})
 
-    logical_cpus = os.cpu_count() or 4
+    logical_cpus = get_cpu_count() or 4
     max_workers = logical_cpus * 4
 
     session = requests.Session()
